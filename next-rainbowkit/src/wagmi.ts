8384023e18e15f8353e665fe3d25e1b42295cdae
Tahlil <bsse0803@iit.du.ac.bd>
2023-07-03 13:41:12 +0600

    Add frontend template

---
+++ next-rainbowkit/src/wagmi.ts
@@ -0,0 +1,28 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const walletConnectProjectId = 'dd11237eff9124157b7d73e82597c582'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My wagmi + RainbowKit App',
+  chains,
+  projectId: walletConnectProjectId,
+})
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
